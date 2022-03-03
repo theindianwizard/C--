50cf29cd26c4895b151c98493f1ed984fc515c85
THEINDIANWIZARD <guttymanswaggg@hotmail.com>
2022-03-03 13:13:23 +0000

    added Language class to methods.h

--- methods.h
+++ methods.h
@@ -1,15 +1,45 @@
 #ifndef Methods_H_INCLUDED
 #define Methods_H_INCLUDED
-#include <iostream>
-using namespace std;
-class Code{
+
+
+#include <iostream> // for cout ,int, string, etc 
+#include <sstream> //to convert string into integers and vice versa
+
+
+using namespace std; //abbreviation
+
+
+class Language{
+
     public:
+
+        int convert(string a){ // converts string to number 
+            istringstream string_to_number(a);
+            int result;
+            string_to_number >> result;
+            return result;
+        }
+
+        string convert(int b){
+            ostringstream number_to_string; 
+            number_to_string << b;
+            return number_to_string.str();
+        }
+};
+
+
+class Code{
+
+    public:
+
         void print(string x){
             cout << x << endl;
         }
+
         void print(int h){
             cout << h << endl;
         }
+        
         void input(string y){
             string z;
             cout << y;
