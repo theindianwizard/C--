4daa58be0ab6bcf68f302e51ff83b206cee030bd
THEINDIANWIZARD <guttymanswaggg@hotmail.com>
2022-03-04 05:14:03 +0000

    made changes for password checker

--- code.cpp
+++ code.cpp
@@ -1,9 +1,26 @@
-#include "methods.h"
 #include <iostream>
+#include <conio.h>
+using namespace std;
 
-int main(){
-    Code python;
-    python.print("Hello World");
-    python.input("What is your name? : ");
-    python.print(5);
+void input(){
+    // Holds the password string.
+    string numAsString = "";
+    // Obtains a single character from the user.
+    char ch = getch();
+    // Keep requesting characters until the user presses
+    // Enter.
+    while (ch != '\r') { // \r is the enter key
+        // Display an asterisk instead of the input character.
+        cout << '*';
+        // Add the character to the password string.
+        numAsString += ch;
+        // Get the next character from the user.
+        ch = getch();
+    }
+    cout << endl;
+    cout << numAsString << endl;
+}
+
+int main() {
+    input();
 }
